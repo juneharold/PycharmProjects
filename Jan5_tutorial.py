efc645b66f4f6e25cc7462c7cd5139edd8c634e1
Charrybot <junehahwang@gmail.com>
2021-01-05 12:08:46 +0900

    corona_edit commit

---
+++ Jan5_tutorial.py
@@ -0,0 +1,12 @@
+"""user = input("enter an integer>>")
+user = int(user)
+
+if user>=10:
+    print("your number is big")
+else:
+    print("your number is small")
+"""
+
+X = input("enter X>>")
+Y = input("enter Y>>")
+Z = input("enter Z>>")
